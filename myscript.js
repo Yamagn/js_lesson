68786f588330afbb26182369aca06f347a304ddf
ymgn <syankenpon@gmail.com>
2018-02-02 00:45:19 +0900

    time lesson

--- myscript.js
+++ myscript.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+/*
+    タイマー処理
+     setInterval
+     setTimeout
+*/
+
+var i = 0;
+
+function show() {
+    console.log(i++);
+    var tid = setTimeout(function() {
+        show();
+    }, 1000);
+    if (i > 3) {
+        clearTimeout(tid);
+    }
+}
+show();
