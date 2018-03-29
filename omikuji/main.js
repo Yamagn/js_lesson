de307403828095a3ad022c0c273c2a58669b841f
Yamagn <syankenpon@gmail.com>
2018-03-29 10:09:20 +0900

    array jissou

--- omikuji/main.js
+++ omikuji/main.js
@@ -4,7 +4,12 @@
     var btn = document.getElementById('btn');
 
     btn.addEventListener('click', function() {
-        var n = Math.floor(Math.random() * 3);
+
+        var results = ['大吉', '中吉', '凶', '末吉'];
+        // var n = Math.floor(Math.random() * 4);
+        var n = Math.floor(Math.random() * results.length);
+        this.textContent = results[n];
+
         // if (n === 0) {
         //     this.textContent = "大吉";
         // } else if (n === 1) {
@@ -13,17 +18,17 @@
         //     this.textContent = "凶";
         // }
 
-        switch (n) {
-            case 0:
-                this.textContent = "大吉";
-                break;
-            case 1:
-                this.textContent = "中吉";
-                break;
-            case 2:
-                this.textContent = "凶";
-                break;
-        }
+        // switch (n) {
+        //     case 0:
+        //         this.textContent = "大吉";
+        //         break;
+        //     case 1:
+        //         this.textContent = "中吉";
+        //         break;
+        //     case 2:
+        //         this.textContent = "凶";
+        //         break;
+        // }
     });
 
     btn.addEventListener('mousedown', function() {
