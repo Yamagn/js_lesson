945b3ac30d4147e699b59508f5827bcf008353b2
Yamagn <syankenpon@gmail.com>
2018-03-29 10:14:40 +0900

    Add kakuritu sousa

--- omikuji/main.js
+++ omikuji/main.js
@@ -5,10 +5,21 @@
 
     btn.addEventListener('click', function() {
 
-        var results = ['大吉', '中吉', '凶', '末吉'];
-        // var n = Math.floor(Math.random() * 4);
-        var n = Math.floor(Math.random() * results.length);
-        this.textContent = results[n];
+        var n = Math.random();
+        if (n < 0.05) {
+            this.textContent = '大吉'; // 5%
+        } else if (n < 0.2) {
+            this.textContent = '中吉'; // 15%
+        } else {
+            this.textContent = '凶';
+        }
+
+        // // var results = ['大吉', '中吉', '凶', '末吉'];
+        // var results = ['大吉','大吉', '大吉', '大吉', '大吉', '凶' ];
+
+        // // var n = Math.floor(Math.random() * 4);
+        // var n = Math.floor(Math.random() * results.length);
+        // this.textContent = results[n];
 
         // if (n === 0) {
         //     this.textContent = "大吉";
